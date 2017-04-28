2494f6eff9b27f493675520147a80cb6854adcd8
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-27 21:29:11 -0500

    loading api data and listening for submit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,56 @@
-console.log("I'm linked");
+$(document).ready(()=>{
+	
+	const apiKey = "";
+
+
+	$("#submit").on("click", () => {
+		let zip = $("#zip").val();
+		loadByZip().then((results) => {
+		//writeForecast();
+		//writeForecast(results.city);
+		console.log(results.city);
+		});
+	});
+
+
+const loadByZip = () => {
+		return new Promise((resolve, reject) => {
+			$.ajax(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&APPID=${apiKey}`)
+			.done((data) => resolve(results))
+			.fail((error) => reject(error));
+		});
+	};
+
+	const loadWeather = () =>{
+		return new Promise((resolve, reject) =>{
+			$.ajax(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${apiKey}`)
+			.done((data) => resolve(data))
+			.fail((error) => reject(error));
+		});
+	};
+
+ const writeForecast = (results) => {
+ 	let outputString = '';
+ 	for (var i = 0; i < results.length; i++) {
+ 		outputString += `<div>${results[i].temp}</div>`;
+ 	}
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
